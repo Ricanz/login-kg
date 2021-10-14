1375447010ce2dcdad969ed770b13e84a7668898
Ruby Trojanovsky <armymadesu@gmail.com>
2021-10-14 10:37:23 +0700

    force potrait, fixed bg di forget pass & register

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -14,10 +14,11 @@ class MainBackground extends StatelessWidget {
       height: size.height,
       width: double.infinity,
       child: Stack(
-        alignment: Alignment.bottomCenter,
         children: [
           Positioned(
-            // top: 0,
+            bottom:0,
+            right: 0,
+            left: 0,
             child: Image(
               image: AssetImage('assets/images/bottom-center.png'),
             ),
